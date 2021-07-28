bb71f18cdd3e0532864fd8136b7b21b25142aa3a
Vamsi Krishna <83570831+vk185170@users.noreply.github.com>
2021-07-28 15:58:59 +0530

    HTTPS Fix required

--- niva.js
+++ niva.js
@@ -1,6 +1,8 @@
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 const { FixFaultTerminalsDialog } = require('./dialogs/fixFaultTerminalsDialog');
 const { IMSupportDialog } = require('./dialogs/imSupportDialog');
+const { FeedbackDialog } = require('./dialogs/feedbackDialog');
+const { CreateTerminalDialog } = require('./dialogs/createTerminalDialog');
 const { LuisRecognizer, QnAMaker } = require('botbuilder-ai');
 
 class NiVA extends ActivityHandler {
@@ -15,6 +17,8 @@ class NiVA extends ActivityHandler {
         // Configuring dialogs
         this.fixFaultTerminalsDialog = new FixFaultTerminalsDialog(this.conversationState, this.userState);
         this.imSupportDialog = new IMSupportDialog(this.conversationState, this.userState);
+        this.feebackDialog = new FeedbackDialog(this.conversationState, this.userState);
+        this.CreateTerminalDialog = new CreateTerminalDialog(this.conversationState, this.userState);
 
         this.previousIntent = this.conversationState.createProperty("previousIntent");
         this.conversationData = this.conversationState.createProperty("conversationData");
@@ -72,7 +76,7 @@ class NiVA extends ActivityHandler {
     }
 
     async sendSuggestedActions(context) {
-        var suggestions = MessageFactory.suggestedActions(['Incident Management Support', 'Fix Terminal Faults', 'I have some Questions', 'I have some Feedback'], 'How may I enlighten your day with?');
+        var suggestions = MessageFactory.suggestedActions(['Incident Management Support', 'Fix Terminal Faults', 'Create a new Terminal', 'I have some Questions', 'I have some Feedback'], 'How may I enlighten your day with?');
         await context.sendActivity(suggestions);
     }
 
@@ -116,8 +120,19 @@ class NiVA extends ActivityHandler {
                     console.log("Inside <Feedback> intent");
                     // TODO
                     await this.conversationData.set(context, { endDialog: false });
-                    await this.fixFaultTerminalsDialog.run(context, this.dialogState, entities);
+                    await this.feebackDialog.run(context, this.dialogState, entities);
                     this.conversationState.endDialog = await this.feebackDialog.isDialogComplete();
+                    if (this.conversationState.endDialog) {
+                        await this.previousIntent.set(context, { intentName: null });
+                        await this.sendSuggestedActions(context);
+                    }
+                    break;
+                case 'Create_Terminal':
+                    console.log("Inside <Create Terminal> intent");
+                    // TODO
+                    await this.conversationData.set(context, { endDialog: false });
+                    await this.CreateTerminalDialog.run(context, this.dialogState, entities);
+                    this.conversationState.endDialog = await this.CreateTerminalDialog.isDialogComplete();
                     if (this.conversationState.endDialog) {
                         await this.previousIntent.set(context, { intentName: null });
                         await this.sendSuggestedActions(context);
